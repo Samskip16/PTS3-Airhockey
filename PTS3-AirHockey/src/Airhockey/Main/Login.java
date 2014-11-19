ca8527daefcadca9a0414932f6558e45f1b173fa
MartijnvDijk <martijn.van.dijk@live.nl>
2014-11-19 14:43:56 +0100

    Login

--- PTS3-AirHockey/src/Airhockey/Main/Login.java
+++ PTS3-AirHockey/src/Airhockey/Main/Login.java
@@ -1,9 +1,24 @@
 package Airhockey.Main;
 
-import java.awt.event.*;
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javax.swing.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,35 +33,92 @@ public class Login extends Application {
 
     Database db;
 
-    JButton blogin = new JButton("Login");
-    JPanel panel = new JPanel();
-    JTextField txuser = new JTextField(15);
-    JPasswordField pass = new JPasswordField(15);
-
+//    JButton blogin = new JButton("Login");
+//    JPanel panel = new JPanel();
+//    JTextField txuser = new JTextField(15);
+//    JPasswordField pass = new JPasswordField(15);
     @Override
     public void start(Stage primaryStage) {
-        Renderer r = new Renderer(primaryStage);
+        Login(primaryStage);
     }
 
-    public Login() {
-//        super("Login Autentification");
-//        this.db = new Database();
-//        setSize(300, 200);
-//        setLocation(500, 280);
-//        panel.setLayout(null);
-//
-//        txuser.setBounds(70, 30, 150, 20);
-//        pass.setBounds(70, 65, 150, 20);
-//        blogin.setBounds(110, 100, 80, 20);
-//
-//        panel.add(blogin);
-//        panel.add(txuser);
-//        panel.add(pass);
-//
-//        getContentPane().add(panel);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setVisible(true);
-//        actionlogin();
+    public void Login(Stage primaryStage) {
+        BorderPane bp = new BorderPane();
+        bp.setPadding(new Insets(10, 50, 50, 50));
+
+        //Adding HBox
+        HBox hb = new HBox();
+        hb.setPadding(new Insets(20, 20, 20, 30));
+
+        //Adding GridPane
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20, 20, 20, 20));
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+
+        //Implementing Nodes for GridPane
+        Label lblUserName = new Label();
+        final TextField txtUserName = new TextField();
+        txtUserName.setText("username");
+        Label lblPassword = new Label();
+        final PasswordField pf = new PasswordField();
+        pf.setText("password");
+        Button btnLogin = new Button("Login");
+        final Label lblMessage = new Label();
+
+        //Adding Nodes to GridPane layout
+        gridPane.add(lblUserName, 0, 0);
+        gridPane.add(txtUserName, 1, 0);
+        gridPane.add(lblPassword, 0, 1);
+        gridPane.add(pf, 1, 1);
+        gridPane.add(btnLogin, 2, 1);
+        gridPane.add(lblMessage, 1, 2);
+
+        //Reflection for gridPane
+        Reflection r = new Reflection();
+        r.setFraction(0.7f);
+        gridPane.setEffect(r);
+
+        //DropShadow effect 
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setOffsetX(5);
+        dropShadow.setOffsetY(5);
+
+        //Adding text and DropShadow effect to it
+        Text text = new Text("  Air Hockey");
+        text.setFont(Font.font("Roboto", FontWeight.BOLD, 28));
+        text.setEffect(dropShadow);
+
+        //Adding text to HBox
+        hb.getChildren().add(text);
+
+        //Action for btnLogin
+        btnLogin.setOnAction(new EventHandler() {
+
+            @Override
+            public void handle(Event event) {
+                String checkUser = txtUserName.getText().toString();
+                String checkPw = pf.getText().toString();
+                if (checkUser.equals("User1") && checkPw.equals("PW")) {
+                    Renderer r = new Renderer(primaryStage);
+                } else {
+                    lblMessage.setText("Incorrect user or pw.");
+                    lblMessage.setTextFill(Color.RED);
+                }
+                txtUserName.setText("");
+                pf.setText("");
+            }
+        });
+
+        //Add HBox and GridPane layout to BorderPane Layout
+        bp.setTop(hb);
+        bp.setCenter(gridPane);
+
+        //Adding BorderPane to the scene and loading CSS
+        Scene scene = new Scene(bp);
+        primaryStage.setScene(scene);
+        //primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
     public void actionlogin() {
